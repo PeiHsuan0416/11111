71a93ea0fad8e684ff5a8f9263d79172543e8e55
hausn <lalalahsuan@gmail.com>
2022-05-01 17:53:10 +0800

    修改刪除功能

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,6 +1,11 @@
 class CandidatesController < ApplicationController
 
     def index
+        @candidates = Candidate.all
+    end
+
+    def show
+        @candidate = Candidate.find_by(id: params[:id])
     end
 
     def new
@@ -8,11 +13,40 @@ class CandidatesController < ApplicationController
     end
 
     def create
-        @candidate = Candidate.new
+        @candidate = Candidate.new(candidate_params)
         if @candidate.save
             redirect_to "/candidates", notice: "新增成功"
         else
             render :new
         end
     end
+
+    def edit
+        @candidate = Candidate.find_by(id: params[:id])
+    end
+
+    def update
+        @candidate = Candidate.find_by(id: params[:id])
+        if @candidate.update(clean_params)
+            redirect_to "/candidates", notice: "更新成功"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @candidate = Candidate.find_by(id: params[:id])
+        @candidate.destroy
+        redirect_to "/candidates", notice: "刪除成功"
+    end
+
+
+    private
+        def find_candidate
+
+        end
+
+        def clean_params
+            params.require(:candidate).permit(:name, :party, :age, :politics)
+        end
 end
